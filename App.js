b62debf88d31849e0684841829111b2af9e6e61c
DaleneyRuben <daleney.fernando@gmail.com>
2025-11-12 23:55:38 -0400

    show splash screen when starting the app

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -6,6 +6,7 @@ import { colors } from './theme';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Platform } from 'react-native';
 import ScreenWrapper from './components/ScreenWrapper';
+import SplashScreen from './components/SplashScreen';
 
 // Import screens
 import HomeScreen from './screens/HomeScreen';
@@ -181,10 +182,21 @@ function RootStackNavigator() {
 }
 
 export default function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  const handleSplashFinish = () => {
+    setShowSplash(false);
+  };
+
   return (
-    <NavigationContainer linking={Platform.OS === 'web' ? linking : undefined}>
-      <RootStackNavigator />
-    </NavigationContainer>
+    <>
+      {showSplash && <SplashScreen onFinish={handleSplashFinish} />}
+      <NavigationContainer
+        linking={Platform.OS === 'web' ? linking : undefined}
+      >
+        <RootStackNavigator />
+      </NavigationContainer>
+    </>
   );
 }
 
